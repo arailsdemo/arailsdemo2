a1d3152f47d2b05154cdcf2559536c06b071192f
aRailsDemo <arailsdemo@gmail.com>
2011-05-15 16:56:58 -0700

    The New Step Definitions

--- external_features/step_definitions/step_list_steps.rb
+++ external_features/step_definitions/step_list_steps.rb
@@ -1,7 +1,29 @@
-Then /^the file "([^"]*)" should contain:$/ do |file, content|
-  check_file_content(file, content, true)
+FIXTURE_PATH = "external_features/support/fixtures"
+
+Given /^existing files with step definitions$/ do
+  files = %w{example_steps1 more/example_steps2}
+  files.each do |file|
+    path = File.expand_path("#{FIXTURE_PATH}/input/#{file}.txt")
+    steps = File.read(path)
+    write_file("features/step_definitions/#{file}.rb", steps)
+  end
 end
 
-Then /^the file "([^"]*)" should not contain:$/ do |file, content|
-  check_file_content(file, content, false)
+Given /^a custom formatter 'StepListFormatter' is defined$/ do
+  formatter_code = File.read("features/support/step_list_formatter.rb")
+  write_file("features/support/step_list_formatter.rb", formatter_code)
+end
+
+Then /^the terminal alphabetically lists the steps with file line numbers$/ do
+  expected_content = File.read("#{FIXTURE_PATH}/output/step_list_output.txt")
+  all_stdout.should match /#{Regexp.escape(expected_content)}/
+end
+
+Then /^a 'step_list\.txt' file is generated with a time stamp$/ do
+  generated_file = "features/step_definitions/step_list.txt"
+  expected_content = File.read("#{FIXTURE_PATH}/output/step_list_output.txt")
+  check_file_content(generated_file, /#{Regexp.escape(expected_content)}/, true)
+
+  date = Time.now.strftime("%a %b %d")
+  check_file_content(generated_file, /#{date}/, true)  # will fail around midnight
 end
